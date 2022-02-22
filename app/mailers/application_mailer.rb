9f7acedf441dd49e2c2957e25ec5e3895145dd92
Muhammad Tayyab <Bcsf17a512@pucit.edu.pk>
2022-02-22 15:39:44 +0500

    add plans to tanats and confirmation styling

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+  user_confirmation_url(:confirmation_token => @token, locale: I18n.locale) %>
 end
